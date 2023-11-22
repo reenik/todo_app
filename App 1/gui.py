8b5174c607e1ada3f8ee4b2e585880b52e3a5244
Nikhil <nikhilsrivastava.rkt@gmail.com>
2023-11-22 11:41:55 +0530

    Day 22-11-23: configured try and cath; changed the theme

--- App 1/gui.py
+++ App 1/gui.py
@@ -1,6 +1,9 @@
 import functions as fun
 import PySimpleGUI as pygui
+import time
 
+pygui.theme("DarkTanBlue")
+# label_clock = pygui.Text("", key="clock")
 label = pygui.Text("Type a to-do:")
 input_box = pygui.InputText(tooltip="Enter a to-do",
                             key="todo")
@@ -14,7 +17,7 @@ edit_button = pygui.Button("Edit list")
 
 complete_button = pygui.Button("Completed")
 exit_button = pygui.Button("EXIT")
-output = pygui.Text(key="output", text_color='black')
+output = pygui.Text(key="output")
 
 window = pygui.Window('My To-Do App',
                       layout=[[label],
@@ -26,6 +29,7 @@ window = pygui.Window('My To-Do App',
 while True:
     event, value = window.read()
     print(window.read())
+    # window["clock"].update(value=time.strftime('%d %B, %Y %H:%M:%S'))
     # print('hello')
     # print('event:', event)
     # print('value:', value)
@@ -39,32 +43,47 @@ while True:
 
             fun.write_todos(user_todos)
             window["todo_items"].update(values=user_todos)
+            window['todo'].update(value="")
 
         case 'Edit list':
-            edited_todo = value["todo_items"][0]
-            new_todo = value["todo"] + '\n'
+            try:
+                edited_todo = value["todo_items"][0]
+                new_todo = value["todo"] + '\n'
 
-            user_todos = fun.get_todos()
-            index = user_todos.index(edited_todo)
-            user_todos[index] = new_todo
+                user_todos = fun.get_todos()
+                index = user_todos.index(edited_todo)
+                user_todos[index] = new_todo
 
-            fun.write_todos(user_todos)
-            window["todo_items"].update(values=user_todos)
+                fun.write_todos(user_todos)
+                window["todo_items"].update(values=user_todos)
+                window['todo'].update(value="")
+
+            except IndexError:
+                pygui.popup("oops 😅 you didn't selected anything to edit.\n"
+                            "Pls select an item from list to edit.",
+                            font=('Comic Sans MS', 14))
 
         case 'Completed':
-            completed_todo = value["todo_items"][0]
+            try:
+                completed_todo = value["todo_items"][0]
 
-            user_todos = fun.get_todos()
+                user_todos = fun.get_todos()
 
-            user_todos.remove(completed_todo)
+                user_todos.remove(completed_todo)
 
-            fun.write_todos(user_todos)
+                fun.write_todos(user_todos)
 
-            completed_todo = completed_todo.strip('\n')
+                completed_todo = completed_todo.strip('\n')
 
-            window['output'].update(value=f"completed '{completed_todo}', hence removed from list. 😊")
-            window["todo_items"].update(values=user_todos)
-            window['todo'].update(value="")
+                window['output'].update(value=f"completed '{completed_todo}', hence removed from list. 😊",
+                                        text_color="black")
+                window["todo_items"].update(values=user_todos)
+                window['todo'].update(value="")
+
+            except IndexError:
+                pygui.popup("oops 😅 you didn't selected anything to be completed.\n"
+                            "Pls select an item from list to complete.",
+                            font=('Comic Sans MS', 14))
 
         case 'todo_items':
             window['todo'].update(value=value["todo_items"][0].strip('\n'))
