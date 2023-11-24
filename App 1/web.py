a48e1e6c141f25b2f5cf4492fd01cc3b703865d1
Nikhil <nikhilsrivastava.rkt@gmail.com>
2023-11-24 14:01:56 +0530

    Day 24-11-23: implement "completed task" features in web.py

--- App 1/web.py
+++ App 1/web.py
@@ -6,9 +6,9 @@ curr_time = time.strftime('%d %B, %Y %H:%M:%S')
 
 user_todos = fun.get_todos()
 
+
 def add_todo():
     new_todo = st.session_state["new_todo"] + "\n"
-    print("-----------------------------------------")
     user_todos.append(new_todo)
     fun.write_todos(user_todos)
     # print(new_todo)
@@ -20,13 +20,16 @@ st.header("This is my Todo App")
 st.write("This app helps to remember the work needs to be done.")
 st.write('Current Time:', curr_time)
 
-# user_todos = fun.get_todos()
-for todo in user_todos:
-    st.checkbox(todo)
+
+for index, todo in enumerate(user_todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        user_todos.pop(index)
+        fun.write_todos(user_todos)
+
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="ABC", placeholder="add a new todo here...",
               on_change=add_todo, key="new_todo", label_visibility="hidden")
 
-print("executed", curr_time, "hello world")
-
-st.session_state
